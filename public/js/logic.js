69aee68f11261f2630baf43cca22b03d15105445
Israel <dragon-zisrae@hotmail.com>
2017-12-06 11:17:39 -0600

    Ready, I guess

--- public/js/logic.js
+++ public/js/logic.js
@@ -13,11 +13,9 @@ $(document).ready(()=>{
 	});
 
 	socket.on('Msg',(m)=>{
-		let htm = '<div class="row">'
-		+'<div class="col-2 font-weight-bold">'+m.user+':</div>'
-		+'<div class="col text-justify">'+m.msg+'</div>'
-		+'</div>';
+		let htm = '<div class="row msg_container base_receive"><div class="col-md-10 col-xs-10"><div class="messages msg_sent"><div class="font-weight-bold">'+m.user+':</div><div class="text-justify">'+m.msg+'</div></div></div></div>';
 		$('#msgs').html($('#msgs').html()+htm);
+		$(".msg_container_base").stop().animate({ scrollTop: $(".msg_container_base")[0].scrollHeight}, 1000);
 	});
 	socket.on('oldMsg',(oldMsgs)=>{
 		if (oldMsgs.length!==0) {
@@ -25,11 +23,12 @@ $(document).ready(()=>{
 			let htm = '';
 			for (var i = 0; i < oldMsgs.length; i++) {
 				let m = oldMsgs[i];
-				htm += '<div class="row">'
-				+'<div class="col-2 font-weight-bold">'+m.user+':</div>'
-				+'<div class="col text-justify">'+m.msg+'</div>'
-				+'</div>';
+				htm += '<div class="row msg_container base_receive"><div class="col-md-10 col-xs-10"><div class="messages msg_sent"><div class="font-weight-bold">'+m.user+':</div><div class="text-justify">'+m.msg+'</div></div></div></div>';
 			}
+			setTimeout(()=>{
+				$(".msg_container_base").stop().animate({ scrollTop: $(".msg_container_base")[0].scrollHeight}, 1000);
+			},500);
+			
 			$('#msgs').html(htm);
 		}
 	});
